7074b8f5785db848da522435cc3b587bbbe6de42
Fernando <slade@kali>
2020-03-11 15:00:03 -0400

    added html code to it

--- web/index.php
+++ web/index.php
@@ -1,3 +1,14 @@
 <?php
-
 phpinfo();
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SimplePHPWebsite</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+</body>
+</html>
